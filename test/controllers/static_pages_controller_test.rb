b4f1592b8bc2cd92e60384466899e2605d42b5ba
stephanie rousset <strabon@club-internet.fr>
2021-02-22 18:52:26 +0100

    finish with static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,41 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # the setup method is called before each test and so @title_part is available for each of the tests.
+  def setup
+    @title_part = "| RoR tuto sample app"
+  end
+  # called after every single test, re-initializing @title_part before every test
+  def teardown
+    @title_part = nil
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home #{@title_part}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help #{@title_part}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About #{@title_part}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact #{@title_part}"
   end
 end
