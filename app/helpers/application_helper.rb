a4876b3dcd072626cfe14a77b9836da353b8cf08
stephanie rousset <strabon@club-internet.fr>
2021-02-23 12:07:39 +0100

    create a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(begin_title = '')
+    end_title = "RoR tuto sample app"
+    if begin_title.empty?
+      end_title
+    else
+      begin_title + " | " + end_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # the setup method is called before each test and so @title_part is available for each of the tests.
   def setup
-    @title_part = "| RoR tuto sample app"
+    @title_part = "RoR tuto sample app"
   end
   # called after every single test, re-initializing @title_part before every test
   def teardown
@@ -18,24 +18,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home #{@title_part}"
+    assert_select "title", "#{@title_part}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help #{@title_part}"
+    assert_select "title", "Help | #{@title_part}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About #{@title_part}"
+    assert_select "title", "About | #{@title_part}"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact #{@title_part}"
+    assert_select "title", "Contact | #{@title_part}"
   end
 end
